d4c47badef1193c2fea264a0fab483fa31b593ee
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-07-27 13:17:32 +0200

    Add pcf for examples

--- point_pattern_example.R
+++ point_pattern_example.R
@@ -1,4 +1,6 @@
 library(helpeR)
+library(onpoint)
+library(patchwork)
 library(spatstat)
 library(tidyverse)
 
@@ -82,7 +84,11 @@ y_max <- 100
 points_coords <- tibble(x = runif(n = n, min = x_min, max = x_max), 
                         y = runif(n = n, min = y_min, max = y_max))
 
-point_pattern_random <- ggplot(data = points_coords) + 
+ppp_random <- ppp(x = points_coords$x, y = points_coords$y, 
+                  window = ripras(x = points_coords$x, y = points_coords$y, 
+                                  shape = "rectangle"))
+
+ppp_random_ggplot <- ggplot(data = points_coords) + 
   geom_point(aes(x = x, y = y), pch = 19, size = 2.5) + 
   geom_rect(aes(xmin = x_min, xmax = x_max, 
                 ymin = y_min, ymax = y_max), 
@@ -92,7 +98,7 @@ point_pattern_random <- ggplot(data = points_coords) +
   theme_void()
 
 save_ggplot(filename = "Figures/point_pattern_random.png",
-            plot = point_pattern_random,
+            plot = ppp_random_ggplot,
             dpi = 300,
             height = 10, width = 10, units = "cm")
 
@@ -106,11 +112,11 @@ x_max <- 100
 y_min <- 0
 y_max <- 100
 
-points_cluster <- rThomas(kappa = 0.001, scale = 5, mu = 20, 
-                          win = owin(xrange = c(x_min, x_max), 
-                                     yrange = c(y_min, y_max)))
+ppp_cluster <- rThomas(kappa = 0.001, scale = 5, mu = 20,
+                       win = owin(xrange = c(x_min, x_max), 
+                                  yrange = c(y_min, y_max)))
 
-point_pattern_clustered <- ggplot(data = as.data.frame(points_cluster)) + 
+ppp_cluster_ggplot <- ggplot(data = as.data.frame(ppp_cluster)) + 
   geom_point(aes(x = x, y = y), pch = 19, size = 2.5) + 
   geom_rect(aes(xmin = x_min, xmax = x_max, 
                 ymin = y_min, ymax = y_max), 
@@ -120,7 +126,7 @@ point_pattern_clustered <- ggplot(data = as.data.frame(points_cluster)) +
   theme_void()
 
 save_ggplot(filename = "Figures/point_pattern_clustered.png",
-            plot = point_pattern_clustered,
+            plot = ppp_cluster_ggplot,
             dpi = 300,
             height = 10, width = 10, units = "cm")
 
@@ -134,11 +140,11 @@ x_max <- 100
 y_min <- 0
 y_max <- 100
 
-points_regular <- rStrauss(beta = 0.02, gamma = 0.1, R = 7.5,
-                            W = owin(xrange = c(x_min, x_max), 
-                                     yrange = c(y_min, y_max)))
+ppp_regular <- rStrauss(beta = 0.02, gamma = 0.1, R = 7.5,
+                        W = owin(xrange = c(x_min, x_max), 
+                                 yrange = c(y_min, y_max)))
 
-point_pattern_regular <- ggplot(data = as.data.frame(points_regular)) + 
+ppp_regular_ggplot <- ggplot(data = as.data.frame(ppp_regular)) + 
   geom_point(aes(x = x, y = y), pch = 19, size = 2.5) + 
   geom_rect(aes(xmin = x_min, xmax = x_max, 
                 ymin = y_min, ymax = y_max), 
@@ -148,7 +154,70 @@ point_pattern_regular <- ggplot(data = as.data.frame(points_regular)) +
   theme_void()
 
 save_ggplot(filename = "Figures/point_pattern_regular.png",
-            plot = point_pattern_regular,
+            plot = ppp_regular_ggplot,
             dpi = 300,
             height = 10, width = 10, units = "cm")
      
+
+#### Summary functions #### 
+
+set.seed(42)
+
+# random
+pcf_csr <- envelope(Y = ppp_random, fun = pcf, 
+                    divisor = "d", nsim = 199, 
+                    correction = "Ripley")
+
+pcf_csr_quantums <- plot_quantums(input = pcf_csr, 
+                                  legend_position = "none", 
+                                  ylab = "g(r)", 
+                                  base_size = 5, 
+                                  line_size = 0.1)
+
+save_ggplot(filename = "Figures/pcf_random.png",
+            plot = pcf_csr_quantums,
+            dpi = 300,
+            height = 10, width = 10, units = "cm")
+
+# clustered
+pcf_cluster <- envelope(Y = ppp_cluster, fun = pcf, 
+                        divisor = "d", nsim = 199, 
+                        correction = "Ripley")
+
+pcf_cluster_quantums <- plot_quantums(input = pcf_cluster, 
+                                      legend_position = "none", 
+                                      ylab = "g(r)", 
+                                      base_size = 5, 
+                                      line_size = 0.1)
+
+save_ggplot(filename = "Figures/pcf_cluster.png",
+            plot = pcf_cluster_quantums,
+            dpi = 300,
+            height = 10, width = 10, units = "cm")
+
+# regular
+pcf_regular <- envelope(Y = ppp_regular, fun = pcf, 
+                        divisor = "d", nsim = 199, 
+                        correction = "Ripley")
+
+pcf_regular_quantums <- plot_quantums(input = pcf_regular, 
+                                      legend_position = "none", 
+                                      ylab = "g(r)", 
+                                      base_size = 5, 
+                                      line_size = 0.1)
+
+save_ggplot(filename = "Figures/pcf_regular.png",
+            plot = pcf_regular_quantums,
+            dpi = 300,
+            height = 10, width = 10, units = "cm")
+
+# combine to one plot
+pcf_overall_quantums <- pcf_cluster_quantums + 
+  pcf_regular_quantums + 
+  pcf_csr_quantums
+
+save_ggplot(filename = "Figures/pcf_overall.png",
+            plot = pcf_overall_quantums,
+            dpi = 300,
+            height = 3, width = 8, units = "cm", overwrite = TRUE)
+
